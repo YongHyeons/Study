2aa9dcb6483544d97c4911d4a831a0131c59167b
YongHyeons <93012448+YongHyeons@users.noreply.github.com>
2024-04-28 23:38:53 +0900

    Add files via upload

---
+++ ProfilePrinter/src/com/project/main/MyProfile.java
@@ -0,0 +1,24 @@
+package com.project.main;
+
+import java.util.Scanner;
+
+public class MyProfile {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int age = scan.nextInt();
+		int weight = scan.nextInt();
+		String name = scan.next();
+		
+		System.out.println(name);
+		System.out.println(age);
+		System.out.println(weight);
+		
+		/*
+		 * 프로젝트명 : Calculator
+		 * 패키지명 : project.main
+		 * 파일명 : MainApp
+		 * 두개의 변수를 이용해서 세개의 숫자를 입력한 후 출력하는 프로그램
+		 * 숫자는 모두 정수
+		 */
+	}
+}
